cedf92d93d2d66be7be2dca076423a11ed2c5082
Joseph Rissman <jirissman@gmail.com>
2025-07-18 16:40:58 -0400

    style(sanity): format code for consistency and readability
    - Resolve `Parameter 'post' implicitly has an 'any' type.` error.

--- astro-app/src/utils/sanity.ts
+++ astro-app/src/utils/sanity.ts
@@ -1,24 +1,29 @@
-import { sanityClient } from 'sanity:client';
-import type { PortableTextBlock } from '@portabletext/types';
-import type { ImageAsset, Slug } from '@sanity/types';
-import groq from 'groq';
+import { sanityClient } from "sanity:client";
+import type { PortableTextBlock } from "@portabletext/types";
+import type { ImageAsset, Slug } from "@sanity/types";
+import groq from "groq";
 
 export async function getPosts() {
-  return await sanityClient.fetch(groq`*[_type == "post" && defined(slug.current)] | order(_createdAt desc)`);
+  return (await sanityClient.fetch(
+    groq`*[_type == "post" && defined(slug.current)] | order(_createdAt desc)`
+  )) as Post[];
 }
 
 export async function getPost(slug: string) {
-  return await sanityClient.fetch(groq`*[_type == "post" && slug.current == $slug][0]`, {
-    slug,
-  });
+  return (await sanityClient.fetch(
+    groq`*[_type == "post" && slug.current == $slug][0]`,
+    {
+      slug,
+    }
+  )) as Post;
 }
 
 export async function getAbout() {
-  return await sanityClient.fetch(groq`*[_type == "about"][0]`);
+  return (await sanityClient.fetch(groq`*[_type == "about"][0]`)) as About;
 }
 
 export interface Post {
-  _type: 'post';
+  _type: "post";
   _createdAt: string;
   title?: string;
   slug: Slug;
@@ -28,7 +33,7 @@ export interface Post {
 }
 
 export interface About {
-  _type: 'about';
+  _type: "about";
   title?: string;
   body: PortableTextBlock[];
   image?: ImageAsset & { alt?: string };
