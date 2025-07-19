a41ddbcc6d4fb0b9b43e18d69f3a387013cbca3e
Joseph Rissman <jirissman@gmail.com>
2025-07-18 22:03:48 -0400

    chore(index): remove unused Hero component export

--- astro-app/src/components/index.ts
+++ astro-app/src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Header } from "./Header.astro";
 export { default as Footer } from "./Footer.astro";
-export { default as Hero } from "./Hero.astro";
 export { default as Card } from "./Card.astro";
