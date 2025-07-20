44d9b193df3871a373f3eb85bd323b6ba9a205e3
Joseph Rissman <jirissman@gmail.com>
2025-07-20 14:49:15 -0400

    fix(ThemeManager): add event listener for theme updates after content swap
    Added a new script to listen for the 'astro:after-swap' event, ensuring that the theme is correctly applied after content updates with ClientRouter. This enhances the user experience by maintaining the selected theme across dynamic content changes.

--- astro-app/src/env.d.ts
+++ astro-app/src/env.d.ts
@@ -1,3 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
+interface Window {
+  theme: {
+    setTheme: (theme: "auto" | "dark" | "light") => void;
+    getTheme: () => "auto" | "dark" | "light";
+    getSystemTheme: () => "light" | "dark";
+    getDefaultTheme: () => "auto" | "dark" | "light";
+  };
+}
